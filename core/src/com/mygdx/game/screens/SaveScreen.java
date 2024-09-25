1058bd4a74bdfa3bab67952f375f2498e5b4f0b5
Mateus <mateusdelluca@gmail.com>
2024-09-25 17:16:05 -0300

    ...

--- core/src/com/mygdx/game/screens/SaveScreen.java
+++ core/src/com/mygdx/game/screens/SaveScreen.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
@@ -77,6 +78,10 @@ public class SaveScreen implements Screen, InputProcessor {
 
     @Override
     public boolean keyDown(int keycode) {
+        if (keycode == Input.Keys.ESCAPE){
+            app.setScreen(app.pauseScreen);
+            Gdx.input.setInputProcessor(app.pauseScreen);
+        }
         return false;
     }
 
